b130bdd79f680c4b5fd8d283c351b989168c4994
Marina Neumann <neumann4@kororaa.cs.pdx.edu>
2017-12-21 15:32:58 -0800

    So it begins. First commit

---
+++ movie.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cctype> 
+#include <cstring>
+#include <cstdlib>
+
+//File holds the movie class, its function and data.
+
+
+
+class movie{
+	public:
+
+	protected:
+
+};
